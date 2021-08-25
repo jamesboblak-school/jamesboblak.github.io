957955484ff06d8648aade8d5e4363eabcca3efe
jamesboblak <james@skepticalrecords.com>
2021-08-25 01:53:52 -0700

    added info to Contact.js and About.js

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "Event Architect",
+      subtitle: "design a party with your party",
+      description:
+        "I worked with two other devolpers to create this Event Coordination application. When you need to organize a party to take to an event - look no further! Inform and involve your guests in advance with <strong>Event Architect.</strong>",
+      image: "./assets/images/event-architect_thumb.png",
+      link: "http://stark-crag-36907.herokuapp.com/",
+    },
+    {
+      title: "Three Word History",
+      subtitle: "Get instant perspective on any three-word story",
+      description:
+        "I worked with a team to develop this outstanding research app. My responisibilities included getting the structure of the page built, including modal display and alert windows, while also making it responsive for smaller screens using Bulma CSS Framework.",
+      image: "./assets/images/three-word-history_demo.jpg",
+      link: "https://jamesboblak.github.io/three-word-history/",
+    },
+    {
+      title: "Weather Dashboard",
+      subtitle: "Get the weather anywhere, including UV Index and 5-day forecast!",
+      description:
+        "Need to know the weather? Try this Weather Dashboard app. I love it! Simply type in a city, and get your weather stats, including UV Index, and a five-day forecast!",
+      image: "./assets/images/weather-dashboard_chicago.jpg",
+      link: "https://jamesboblak.github.io/weather-dashboard/",
+    },
+    {
+      title: "Day Planner App",
+      subtitle: "Easily schedule your day",
+      description:
+        "Daily planning is easy with this handy app! I use it!! Feel free to drop your daily events into it - it will remember until you delete the events, even if you refresh the page",
+      image: "./assets/images/day-planner_demo.jpg",
+      link: "https://jamesboblak.github.io/day-planner/",
+    },
+  ];
