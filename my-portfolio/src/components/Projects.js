247dfd26c6b5f04fc2a6b965aa0426aa810fe0fe
jamesboblak <james@skepticalrecords.com>
2021-08-25 15:31:49 -0700

    added footer

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -12,9 +12,6 @@ export default function Projects() {
           <h2 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Apps I've Built
           </h2>
-          {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            I've had the pleasure to build several apps on my own, and a few with a team.  Pleaes feel free to try the apps out and contact me with any interest or questions.
-          </p> */}
         </div>
         <div className="flex flex-wrap -m-4" id="card1">
           {projects.map((project) => (
@@ -30,12 +27,12 @@ export default function Projects() {
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                  <h1 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.title}
                   </h1>
+                  <h2 className="title-font text-lg font-medium text-white mb-3">
+                    {project.subtitle}
+                  </h2>
                   <p className="leading-relaxed">{project.description}</p>
                 </div>
               </div>
--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -6,6 +6,7 @@ export const projects = [
         "I worked with two other devolpers to create this Event Coordination application. When you need to organize a party to take to an event - look no further! Inform and involve your guests in advance with Event Architect.",
       image: "/assets/images/event-architect_thumb.png",
       link: "http://stark-crag-36907.herokuapp.com/",
+      // linkRepo: "https://github.com/jamesboblak/three-word-history",
     },
     {
       title: "Three Word History",
@@ -14,6 +15,7 @@ export const projects = [
         "I worked with a team to develop this outstanding research app. My responisibilities included getting the structure of the page built, including modal display and alert windows, while also making it responsive for smaller screens using Bulma CSS Framework.",
       image: "./assets/images/three-word-history_demo.jpg",
       link: "https://jamesboblak.github.io/three-word-history/",
+      // linkRepo: "https://github.com/jamesboblak/three-word-history",
     },
     {
       title: "Weather Dashboard",
@@ -30,5 +32,21 @@ export const projects = [
         "Daily planning is easy with this handy app! I use it!! Feel free to drop your daily events into it - it will remember until you delete the events, even if you refresh the page",
       image: "./assets/images/day-planner_demo.jpg",
       link: "https://jamesboblak.github.io/day-planner/",
+    },
+    {
+      title: "Password Generator App",
+      subtitle: "Easily schedule your day",
+      description:
+        "A handy password generator that will quickly make you a password from 8 to 128 upper-case, lower-case, numerical, and/or special characters.",
+      image: "./assets/images/password-generator_demo.jpg",
+      link: "https://jamesboblak.github.io/password-generator/",
+    },
+    {
+      title: "RegEx Hex Finder",
+      subtitle: "Successfully parse 3- and 6-digit Hexadecimal data!",
+      description:
+        "Matching a Hex Value is useful when working with hex data, such as RGB color data. You'll always want to make sure you have your data formatted correctly, or unexpected errors could be caused by critical data syntax formatting that is not easily readable with the untrained eye. Regex can also be useful for finding specific data within a document. As an example, we will use this Regex expression to find all the colors within a CSS document.",
+      image: "./assets/images/RegEx_demo.png",
+      link: "https://gist.github.com/jamesboblak/dbc842d499d9baa02188751152f583b6",
     },
   ];
